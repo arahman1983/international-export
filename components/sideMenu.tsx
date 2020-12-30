fac6eb7af7dfe980e4b9f00d939bbdeb8ae5645a
Abdelrahmany Elhussiny <a.rahman1783@yahoo.com>
2020-12-30 13:35:53 +0200

    active sidemenu

--- components/sideMenu.tsx
+++ components/sideMenu.tsx
@@ -3,20 +3,17 @@ import Link from 'next/link'
 import { faTimes, faBars, faUsers, faPuzzlePiece, faCopyright, faCar, faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from '../styles/sideMenu.module.css'
+import { useRouter } from 'next/router'
 
 export default function SideMenu() {
   const [collapsed, setCollapsed] = useState(false)
+  const router = useRouter()
 
   const toggleMenu = (e:React.MouseEvent<HTMLButtonElement>) => {
     setCollapsed(!collapsed)
   }
 
   const menuItems = [
-    {
-      title: 'Users',
-      link: '/admin/users',
-      icon: faUsers
-    },
     {
       title: 'Categories',
       link: '/admin/categories',
@@ -36,6 +33,11 @@ export default function SideMenu() {
       title: 'Contact Us',
       link: '/admin/contacts',
       icon: faPhoneAlt
+    },
+    {
+      title: 'Users',
+      link: '/admin/users',
+      icon: faUsers
     }
 
   ]
@@ -48,7 +50,7 @@ export default function SideMenu() {
       <ul className={styles.sideMenuUl}>
         {
           menuItems.map((item,i)=> (
-            <li className={styles.sideItem} key={i}>
+            <li className={`${router.pathname === item.link ? styles.sideItemActive : styles.sideItem}`} key={i}>
               <Link href={item.link}>
                 <a className={styles.sideLink}> 
                   <FontAwesomeIcon icon={item.icon}
