4d0f2a8ceea0964e9e4603942995036fa1718bc3
Abdelrahman Elhussiny <Abdelrahman Elhussiny>
2021-01-05 16:21:11 +0200

    fix about API

--- pages/api/about/about.ts
+++ pages/api/about/about.ts
@@ -1,18 +1,30 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { query } from '../../../lib/db'
+import { NextApiRequest, NextApiResponse } from "next";
+import { query } from "../../../lib/db";
 
-export default async function getAbout(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function getAbout(req: NextApiRequest, res: NextApiResponse) {
+  let lang = req.query.lang;
   try {
-    const results = await query(`
-      SELECT * FROM about
-      ORDER BY id DESC
-      LIMIT 1
-  `)
-    return res.json(results)
+    let results;
+    lang
+      ? lang === "ar"
+        ? (results = await query(`
+              SELECT {title_ar, description_ar, details_ar, image, keyWords} FROM about
+              ORDER BY id DESC
+              LIMIT 1
+          `))
+        : (results = await query(`
+              SELECT {title, description, details, image, keyWords} FROM about
+              ORDER BY id DESC
+              LIMIT 1
+          `))
+      : (results = await query(`
+            SELECT * FROM about
+            ORDER BY id DESC
+            LIMIT 1
+        `));
+
+    return res.json(results);
   } catch (e) {
-    res.status(500).json({ message: e.message })
+    res.status(500).json({ message: e.message });
   }
-};
+}
