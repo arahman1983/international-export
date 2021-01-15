20e9ce04cc8b9dc7a1082ec27bb28ed4baa8cb10
Abdelrahmany Elhussiny <a.rahman1783@yahoo.com>
2021-01-16 00:26:03 +0200

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,18 @@ import { useRouter } from 'next/router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import { Loader } from '../components'
+import RUM from 'next-rum';
 
+/**
+ * Implement your custom tracker/analytics here to receive the RUM data.
+ *
+ * @param {String} url The URL that we navigated to.
+ * @param {Object} rum The measured navigation data.
+ * @private
+ */
+function navigated(url, rum) {
+  console.log('the page has navigated to', url, rum);
+}
 
 function MyApp({ Component, pageProps }) {
   const Router = useRouter()
@@ -21,9 +32,11 @@ function MyApp({ Component, pageProps }) {
   }, [])
 
   return isLoaded ? (
+    <RUM navigated={ navigated }>
       <LanguageProvider>
         <Component {...pageProps} />
       </LanguageProvider>
+    </RUM>
       ) : <Loader />
 }
 
